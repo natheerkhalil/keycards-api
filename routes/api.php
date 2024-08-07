35be675986b25404a79aa42738f5674eeb7ba22d
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-07 11:42:47 +0100

    Remove double Route::, combined into group

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('throttle:30,1')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
 });
 
-Route::middleware('throttle: 200,1')->group(function () {
+Route::middleware('throttle:200,1')->group(function () {
     Route::post('/validation/login', [AuthController::class, 'validateLogin']);
     Route::post('/validation/register', [AuthController::class, 'validateRegister']);
 
@@ -27,41 +27,39 @@ Route::middleware('throttle: 200,1')->group(function () {
     Route::post('/video/lyrics', [VideoController::class, 'lyrics']);
 });
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::middleware('throttle:70,1')->group(function () {
-        // videos
-        Route::post('/video/save', [VideoController::class, 'save']);
-        Route::post('/video/fetch', [VideoController::class, 'fetch']);
-        Route::post('/video/load', [VideoController::class, 'load']);
-        Route::post('/video/fav', [VideoController::class, 'fav']);
-        Route::post('/video/all', [VideoController::class, 'all']);
-        Route::post('/video/favs', [VideoController::class, 'favs']);
-        Route::post('/video/delete', [VideoController::class, 'delete']);
-        Route::post('/video/random', [VideoController::class, 'random']);
-        Route::post('/video/playlist', [VideoController::class, 'playlist']);
+Route::group(["middleware" => ["auth:sanctum", "throttle:5,1"]], function () {
+    // videos
+    Route::post('/video/save', [VideoController::class, 'save']);
+    Route::post('/video/fetch', [VideoController::class, 'fetch']);
+    Route::post('/video/load', [VideoController::class, 'load']);
+    Route::post('/video/fav', [VideoController::class, 'fav']);
+    Route::post('/video/all', [VideoController::class, 'all']);
+    Route::post('/video/favs', [VideoController::class, 'favs']);
+    Route::post('/video/delete', [VideoController::class, 'delete']);
+    Route::post('/video/random', [VideoController::class, 'random']);
+    Route::post('/video/playlist', [VideoController::class, 'playlist']);
 
-        // playlist
-        Route::post('/playlist/create', [PlaylistController::class, 'create']);
-        Route::post('/playlist/update', [PlaylistController::class, 'update']);
-        Route::post('/playlist/delete', [PlaylistController::class, 'delete']);
-        Route::post('/playlist/videos', [PlaylistController::class, 'delete']);
-        Route::post('/playlist/list', [PlaylistController::class, 'list']);
-        Route::post('/playlist/add', [PlaylistController::class, 'add']);
-        Route::post('/playlist/remove', [PlaylistController::class, 'remove']);
+    // playlist
+    Route::post('/playlist/create', [PlaylistController::class, 'create']);
+    Route::post('/playlist/update', [PlaylistController::class, 'update']);
+    Route::post('/playlist/delete', [PlaylistController::class, 'delete']);
+    Route::post('/playlist/videos', [PlaylistController::class, 'delete']);
+    Route::post('/playlist/list', [PlaylistController::class, 'list']);
+    Route::post('/playlist/add', [PlaylistController::class, 'add']);
+    Route::post('/playlist/remove', [PlaylistController::class, 'remove']);
 
-        // shares
-        Route::post('/share/create', [ShareController::class, 'create']);
-        Route::post('/share/respond', [ShareController::class, 'respond']);
-        Route::post('/share/list', [ShareController::class, 'list']);
+    // shares
+    Route::post('/share/create', [ShareController::class, 'create']);
+    Route::post('/share/respond', [ShareController::class, 'respond']);
+    Route::post('/share/list', [ShareController::class, 'list']);
 
-        // account
-        Route::post('/account/delete', [AuthController::class, 'deleteAccount']);
-        Route::post('/account/is-verified', [AuthController::class, 'isVerified']);
-        Route::post('/account/send-verification-email', [AuthController::class, 'sendVerificationEmail']);
-        Route::post('/account/verify-token', [AuthController::class, 'verifyToken']);
-        Route::post('/account/send-email-change-email', [AuthController::class, 'sendEmailChangeEmail']);
-        Route::post('/account/change-email', [AuthController::class, 'changeEmail']);
-    });
+    // account
+    Route::post('/account/delete', [AuthController::class, 'deleteAccount']);
+    Route::post('/account/is-verified', [AuthController::class, 'isVerified']);
+    Route::post('/account/send-verification-email', [AuthController::class, 'sendVerificationEmail']);
+    Route::post('/account/verify-token', [AuthController::class, 'verifyToken']);
+    Route::post('/account/send-email-change-email', [AuthController::class, 'sendEmailChangeEmail']);
+    Route::post('/account/change-email', [AuthController::class, 'changeEmail']);
 });
 
 Route::middleware('throttle:60,1')->group(function () {
