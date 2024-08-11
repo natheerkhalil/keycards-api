1840979148e184e119cbb3c08101ba375e5ea0a6
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-11 13:41:27 +0100

    Add config files for env variables for 3rd party apis

---
+++ config/hcaptcha.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'secret_key' => env('HCAPTCHA_SECRET'),
+];
---
+++ config/video.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "yt_api_key" => env("YT_API_KEY"),
+
+    "genius_access_token" => env("GENIUS_ACCESS_TOKEN"),
+];
