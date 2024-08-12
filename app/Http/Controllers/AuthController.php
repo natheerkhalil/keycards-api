18ceb504328739c702fcaadc075c173d9c6fd114
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-12 18:00:17 +0100

    Fix comparison for placeholder token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,10 +45,10 @@ class AuthController extends Controller
     // VERIFY CAPTCHA TOKEN
     private function verifyCaptcha($token)
     {
-        if ($token == \Config::get('captcha.secret_key')) {
+        if ($token == \Config::get('captcha.placeholder_token')) {
             return true;
         }
-        
+
         // Cloudflare Turnstile verification URL
         $url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
 
